ab6278dac3f6f2811b2368dfca981d7d1be9ce45
Sumjoy2 <5011286mountjoy@gmail.com>
2023-10-18 11:46:17 -0700

    changePushTesting

---
+++ Assets/Scrip/Light Flicker.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // = GameObject.FindWithTag("Light");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
