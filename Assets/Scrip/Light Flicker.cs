9733c552f2d24b0961f137973a5ca04a3c752bc5
Blake Schutiz <Blakeduane1120@gmail.com>
2023-10-18 13:35:53 -0700

    jonny

--- Assets/Scrip/Light Flicker.cs
+++ Assets/Scrip/Light Flicker.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class LightFlicker : MonoBehaviour
 {
+    public float flickerInrensity = 0.2f;
+    public float flickerspersecond = 3.0f;
+    public float speedRandomness = 1.0f;
 
+    private float time;
+    private float startingIntensity;
+    private Light lights;
     // Start is called before the first frame update
     void Start()
     {
-        // = GameObject.FindWithTag("Light");
+        lights = GetComponent<Light>();
+        startingIntensity = lights.intensity;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        time += Time.deltaTime * (1 - Random.Range(-speedRandomness, speedRandomness)) * Mathf.PI;
+        lights.intensity = startingIntensity + Mathf.Sin(time * flickerspersecond) * flickerspersecond;
     }
 }
