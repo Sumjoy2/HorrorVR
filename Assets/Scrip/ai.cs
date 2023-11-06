df3bf59af60a1cd9fe3183d2983d79f23e93e068
Blake Schutiz <Blakeduane1120@gmail.com>
2023-11-06 13:09:09 -0800

    biueqgfoiuh

---
+++ Assets/Scrip/ai.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ai : MonoBehaviour
+{
+    public NavMeshAgent enemy;
+    public Transform player;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        enemy.SetDestination(player.position);
+    }
+}
